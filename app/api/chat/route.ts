e38747c0b36b2e3214418818b8de9f912d94548a
melienherrera <melienherrera@gmail.com>
2024-01-18 10:27:58 -0800

    Route page updated
    Tried Astra DB connection and querying database

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,7 +1,7 @@
-import OpenAI from 'openai';
-import { OpenAIStream, StreamingTextResponse } from 'ai';
-import {AstraDB} from "@datastax/astra-db-ts";
-
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import { AstraDB } from "@datastax/astra-db-ts";
+import { GoogleVertexAIMultimodalEmbeddings } from "langchain/experimental/multimodal_embeddings/googlevertexai";
 
 const {
   ASTRA_DB_ENDPOINT,
@@ -14,21 +14,53 @@ const {
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
   apiKey: OPENAI_API_KEY,
+  // Not working for some reason
+  // baseURL: "https://open-assistant-ai.astra.datastax.com/v1",
+  // defaultHeaders: {
+  //   "astra-api-token": ASTRA_DB_APPLICATION_TOKEN,
+  // }
 });
- 
+
 // IMPORTANT! Set the runtime to edge
-export const runtime = 'edge';
- 
+export const runtime = "edge";
+
+// Establish connection with database
+// const astraDb = new AstraDB(ASTRA_DB_APPLICATION_TOKEN, ASTRA_DB_ENDPOINT);
+
 export async function POST(req: Request) {
   // 'data' contains the additional data that you have sent:
   const { messages, data } = await req.json();
- 
+
+  // Initialize GoogleVertexAIMultimodalEmbeddings and OpenAI
+  // const vertexEmbed = new GoogleVertexAIMultimodalEmbeddings();
+
+  // Query the DB
+  /*
+  try {
+    const collection = await astraDb.collection(ASTRA_DB_COLLECTION);
+    const embedding = await vertexEmbed.embedImageQuery(data.imageUrl);
+    const cursor = collection
+      .find(null, {
+        sort: {
+          $vector: embedding,
+        },
+        limit: 3,
+      })
+      .toArray()
+      .then((docs) => {
+        docs.forEach((doc) => console.log(doc));
+      });
+  } catch (e) {
+    console.log("Error querying db...");
+  }
+  */
+
   const initialMessages = messages.slice(0, -1);
   const currentMessage = messages[messages.length - 1];
- 
+
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
-    model: 'gpt-4-vision-preview',
+    model: "gpt-4-vision-preview",
     stream: true,
     max_tokens: 150,
     messages: [
@@ -36,85 +68,20 @@ export async function POST(req: Request) {
       {
         ...currentMessage,
         content: [
-          { type: 'text', text: currentMessage.content },
- 
+          { type: "text", text: currentMessage.content },
+
           // forward the image information to OpenAI:
           {
-            type: 'image_url',
+            type: "image_url",
             image_url: data.imageUrl,
           },
         ],
       },
     ],
   });
-  
 
   // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response);
   // Respond with the stream
   return new StreamingTextResponse(stream);
 }
-
-// //Import necessary modules
-// // import fs from "fs";
-// import path from "path";
-// // import { GoogleVertexAIMultimodalEmbeddings } from "langchain/experimental/multimodal_embeddings/googlevertexai";
-// // import { FaissStore } from "langchain/vectorstores/faiss";
-// // import { Document } from "langchain/document";
-// import OpenAI from "openai";
-// import { OpenAIStream, StreamingTextResponse } from "ai";
-// import { AstraDB } from "@datastax/astra-db-ts";
-
-// const {
-//   ASTRA_DB_ENDPOINT,
-//   ASTRA_DB_APPLICATION_TOKEN,
-//   ASTRA_DB_NAMESPACE,
-//   ASTRA_DB_COLLECTION,
-//   COHERE_API_KEY,
-//   OPENAI_API_KEY,
-// } = process.env;
-
-// //Initialize GoogleVertexAIMultimodalEmbeddings and OpenAI
-// //const vertexEmbed = new GoogleVertexAIMultimodalEmbeddings();
-
-// const openai = new OpenAI({
-//   apiKey: OPENAI_API_KEY,
-//   baseURL: "https://open-assistant-ai.astra.datastax.com/v1",
-//   defaultHeaders: {
-//     "astra-api-token": ASTRA_DB_APPLICATION_TOKEN,
-//   },
-// });
-
-// export const runtime = "edge";
-
-// // Establish connection with Astra DB
-// const astraDb = new AstraDB(ASTRA_DB_APPLICATION_TOKEN, ASTRA_DB_ENDPOINT);
-
-// Set example image
-// const exampleImage = fs.readFileSync("app/assets/example-outfit.jpeg");
-
-// // generate image embedding
-// const generateImageEmbedding = async (img) => {
-//   console.log("Generating image embeddings");
-//   const embeddedImage = await vertexEmbed.embedImageQuery(img);
-//   return embeddedImage;
-// };
-
-
-// const findSimilarItems = async (img) => {
-//   try {
-//     const collection = await astraDb.collection(ASTRA_DB_COLLECTION);
-//     const embedding = await vertexEmbed.embedImageQuery(img);
-//     const cursor = collection.find(null, {
-//       sort: {
-//         $vector: embedding,
-//       },
-//       limit: 3,
-//     }).toArray().then(docs => {docs.forEach(doc => console.log(doc))});
-
-//   } catch (e) {
-//     console.log("Error querying db...");
-//   }
-// };
-
-// findSimilarItems(exampleImage)
