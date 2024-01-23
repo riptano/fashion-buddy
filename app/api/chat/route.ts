4e3b882d1b6248add952f1255bc212f6ff23b631
Annie Phan <annie.phan@datastax.com>
2024-01-23 15:14:25 -0600

    started from scratch. still getting module not found cannot resolve 'os' when adding astra connection

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,87 +1,38 @@
-import OpenAI from "openai";
-import { OpenAIStream, StreamingTextResponse } from "ai";
-import { AstraDB } from "@datastax/astra-db-ts";
-import { GoogleVertexAIMultimodalEmbeddings } from "langchain/experimental/multimodal_embeddings/googlevertexai";
+import { AstraDB } from '@datastax/astra-db-ts';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import { GoogleGenerativeAIStream, Message, StreamingTextResponse } from 'ai';
+ 
+const { ASTRA_DB_APPLICATION_TOKEN, ASTRA_DB_ENDPOINT } = process.env;
+console.log(ASTRA_DB_ENDPOINT);
+const db = new AstraDB(ASTRA_DB_APPLICATION_TOKEN, ASTRA_DB_API_ENDPOINT);
 
-const {
-  ASTRA_DB_ENDPOINT,
-  ASTRA_DB_APPLICATION_TOKEN,
-  ASTRA_DB_NAMESPACE,
-  ASTRA_DB_COLLECTION,
-  OPENAI_API_KEY,
-} = process.env;
-
-// Create an OpenAI API client (that's edge friendly!)
-const openai = new OpenAI({
-  apiKey: OPENAI_API_KEY,
-  // Not working for some reason
-  // baseURL: "https://open-assistant-ai.astra.datastax.com/v1",
-  // defaultHeaders: {
-  //   "astra-api-token": ASTRA_DB_APPLICATION_TOKEN,
-  // }
-});
-
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY || '');
+ 
 // IMPORTANT! Set the runtime to edge
-export const runtime = "edge";
-
-// Establish connection with database
-// const astraDb = new AstraDB(ASTRA_DB_APPLICATION_TOKEN, ASTRA_DB_ENDPOINT);
-
+export const runtime = 'edge';
+ 
+// convert messages from the Vercel AI SDK Format to the format
+// that is expected by the Google GenAI SDK
+const buildGoogleGenAIPrompt = (messages: Message[]) => ({
+    contents: messages
+        .filter(message => message.role === 'user' || message.role === 'assistant')
+        .map(message => ({
+            role: message.role === 'user' ? 'user' : 'model',
+            parts: [{ text: message.content }],
+    })),
+});
+ 
 export async function POST(req: Request) {
-  // 'data' contains the additional data that you have sent:
-  const { messages, data } = await req.json();
+    // Extract the `prompt` from the body of the request
+    const { messages } = await req.json();
 
-  // Initialize GoogleVertexAIMultimodalEmbeddings and OpenAI
-  // const vertexEmbed = new GoogleVertexAIMultimodalEmbeddings();
+    const geminiStream = await genAI
+        .getGenerativeModel({ model: 'gemini-pro' })
+        .generateContentStream(buildGoogleGenAIPrompt(messages));
 
-  // Query the DB
-  /*
-  try {
-    const collection = await astraDb.collection(ASTRA_DB_COLLECTION);
-    const embedding = await vertexEmbed.embedImageQuery(data.imageUrl);
-    const cursor = collection
-      .find(null, {
-        sort: {
-          $vector: embedding,
-        },
-        limit: 3,
-      })
-      .toArray()
-      .then((docs) => {
-        docs.forEach((doc) => console.log(doc));
-      });
-  } catch (e) {
-    console.log("Error querying db...");
-  }
-  */
+    // Convert the response into a friendly text-stream
+    const stream = GoogleGenerativeAIStream(geminiStream);
 
-  const initialMessages = messages.slice(0, -1);
-  const currentMessage = messages[messages.length - 1];
-
-  // Ask OpenAI for a streaming chat completion given the prompt
-  const response = await openai.chat.completions.create({
-    model: "gpt-4-vision-preview",
-    stream: true,
-    max_tokens: 150,
-    messages: [
-      ...initialMessages,
-      {
-        ...currentMessage,
-        content: [
-          { type: "text", text: currentMessage.content },
-
-          // forward the image information to OpenAI:
-          {
-            type: "image_url",
-            image_url: data.imageUrl,
-          },
-        ],
-      },
-    ],
-  });
-
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
-  // Respond with the stream
-  return new StreamingTextResponse(stream);
+    // Respond with the stream
+    return new StreamingTextResponse(stream);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,26 @@
-// "use client";
-// import { useState } from "react";
-
 'use client';
-
-import { useChat } from "ai/react";
-
+ 
+import { useChat } from 'ai/react';
+ 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
-
-  // image URL now >> change this to image upload
-  const imageUrl =
-    "https://i.pinimg.com/564x/47/18/81/4718815109cd59904c109b734c27ed4a.jpg";
-
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      <img alt="preview image" src={imageUrl} />
-      {messages.length > 0
-        ? messages.map((m) => (
-            <div key={m.id} className="whitespace-pre-wrap">
-              {m.role === "user" ? "User: " : "AI: "}
-              {m.content}
-            </div>
-          ))
-        : null}
-      <form
-        onSubmit={(e) => {
-          handleSubmit(e, {
-            data: {
-              imageUrl: imageUrl,
-            },
-          });
-        }}
-      >
+      {messages.map(m => (
+        <div key={m.id} className="whitespace-pre-wrap">
+          {m.role === 'user' ? 'User: ' : 'AI: '}
+          {m.content}
+        </div>
+      ))}
+ 
+      <form onSubmit={handleSubmit}>
         <input
           className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
           value={input}
-          placeholder="Ask a question about the image..."
+          placeholder="Say something..."
           onChange={handleInputChange}
         />
       </form>
     </div>
   );
 }
-
-// export default function Home() {
-//   const [uploadedImage, setImage] = useState(null);
-
-//   const onImageChange = (event) => {
-//     if (event.target.files && event.target.files[0]) {
-//       const i = event.target.files[0];
-//       setImage(URL.createObjectURL(i));
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <input type="file" onChange={onImageChange} />
-//       <img alt="preview image" src={uploadedImage} />
-//     </div>
-//   );
-
-// }
