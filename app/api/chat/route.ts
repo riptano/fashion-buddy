278c37b40a3eaa5897ea0fb3d2670e02233e8f27
Annie Phan <annie.phan@datastax.com>
2024-01-24 12:35:20 -0600

    add sample outfit and connect to gemini pro vision

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,38 +1,49 @@
 import { AstraDB } from '@datastax/astra-db-ts';
 import { GoogleGenerativeAI } from '@google/generative-ai';
-import { GoogleGenerativeAIStream, Message, StreamingTextResponse } from 'ai';
- 
-const { ASTRA_DB_APPLICATION_TOKEN, ASTRA_DB_ENDPOINT } = process.env;
-console.log(ASTRA_DB_ENDPOINT);
-const db = new AstraDB(ASTRA_DB_APPLICATION_TOKEN, ASTRA_DB_API_ENDPOINT);
+import { GoogleGenerativeAIStream, OpenAIStream, Message, StreamingTextResponse } from 'ai';
+import fs from 'fs';
 
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY || '');
- 
-// IMPORTANT! Set the runtime to edge
-export const runtime = 'edge';
- 
-// convert messages from the Vercel AI SDK Format to the format
-// that is expected by the Google GenAI SDK
-const buildGoogleGenAIPrompt = (messages: Message[]) => ({
-    contents: messages
-        .filter(message => message.role === 'user' || message.role === 'assistant')
-        .map(message => ({
-            role: message.role === 'user' ? 'user' : 'model',
-            parts: [{ text: message.content }],
-    })),
-});
- 
+
+const { 
+    ASTRA_DB_APPLICATION_TOKEN, 
+    ASTRA_DB_ENDPOINT, 
+    GOOGLE_API_KEY,
+} = process.env;
+
+// Connect to Astra
+const db = new AstraDB(ASTRA_DB_APPLICATION_TOKEN, ASTRA_DB_ENDPOINT);
+
+// Connect to Google GenAI
+const genAI = new GoogleGenerativeAI(GOOGLE_API_KEY || '');
+const gemini_model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+
+// Converts local file information to a GoogleGenerativeAI.Part object.
+function fileToGenerativePart(path: fs.PathOrFileDescriptor, mimeType: string) {
+    const part = {
+        inlineData: {
+        data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+        mimeType
+        },
+    };
+    return part;
+}
+
 export async function POST(req: Request) {
     // Extract the `prompt` from the body of the request
-    const { messages } = await req.json();
+    const { messages, data } = await req.json();
 
-    const geminiStream = await genAI
-        .getGenerativeModel({ model: 'gemini-pro' })
-        .generateContentStream(buildGoogleGenAIPrompt(messages));
+    const currentMessage = messages[messages.length - 1];
+    const prompt = currentMessage.content;
+    const image = fileToGenerativePart("app/sample_outfit.jpeg", "image/jpeg");
+    const textPart = {text: prompt};
+    const request = {
+        contents: [{role: 'user', parts: [textPart, image]}],
+    };
+    
+    const geminiStream = await gemini_model.generateContentStream(request);
 
     // Convert the response into a friendly text-stream
     const stream = GoogleGenerativeAIStream(geminiStream);
-
     // Respond with the stream
     return new StreamingTextResponse(stream);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,35 @@ import { useChat } from 'ai/react';
  
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
+ 
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map(m => (
-        <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === 'user' ? 'User: ' : 'AI: '}
-          {m.content}
-        </div>
-      ))}
+      <p>Sample Image</p>
+      <img 
+      src='https://cdn.cliqueinc.com/posts/298692/streetwear-outfits-for-women-298692-1648082641136-main.700x0c.jpg'/>
+      {messages.length > 0
+        ? messages.map(m => (
+            <div key={m.id} className="whitespace-pre-wrap">
+              {m.role === 'user' ? 'User: ' : 'AI: '}
+              {m.content}
+            </div>
+          ))
+        : null}
  
-      <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={e => {
+          handleSubmit(e, {
+            data: {
+              imageUrl:
+                'https://cdn.cliqueinc.com/posts/298692/streetwear-outfits-for-women-298692-1648082641136-main.700x0c.jpg',
+            },
+          });
+        }}
+      >
         <input
           className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
           value={input}
-          placeholder="Say something..."
+          placeholder="Ask a question about this image"
           onChange={handleInputChange}
         />
       </form>
