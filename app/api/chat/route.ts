69febb7f24f7ecc71cbaf140f6e246d34ffef05d
melienherrera <melienherrera@gmail.com>
2024-01-24 15:40:37 -0800

    Updated to pass image from file upload into gemini model

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,49 +1,60 @@
-import { AstraDB } from '@datastax/astra-db-ts';
-import { GoogleGenerativeAI } from '@google/generative-ai';
-import { GoogleGenerativeAIStream, StreamingTextResponse } from 'ai';
-import fs from 'fs';
+import { AstraDB } from "@datastax/astra-db-ts";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenerativeAIStream, StreamingTextResponse } from "ai";
+import fs from "fs";
 
-
-const { 
-    ASTRA_DB_APPLICATION_TOKEN, 
-    ASTRA_DB_ENDPOINT, 
-    GOOGLE_API_KEY,
-} = process.env;
+// 
+const { ASTRA_DB_APPLICATION_TOKEN, ASTRA_DB_ENDPOINT, GOOGLE_API_KEY } =
+  process.env;
 
 // Connect to Astra
 const db = new AstraDB(ASTRA_DB_APPLICATION_TOKEN, ASTRA_DB_ENDPOINT);
 
 // Connect to Google GenAI
-const genAI = new GoogleGenerativeAI(GOOGLE_API_KEY || '');
+const genAI = new GoogleGenerativeAI(GOOGLE_API_KEY || "");
 const gemini_model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
 
-// Converts local file information to a GoogleGenerativeAI.Part object.
+
 function fileToGenerativePart(path: fs.PathOrFileDescriptor, mimeType: string) {
-    const part = {
-        inlineData: {
-        data: Buffer.from(fs.readFileSync(path)).toString("base64"),
-        mimeType
-        },
-    };
-    return part;
+  const part = {
+    inlineData: {
+      data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+      mimeType,
+    },
+  };
+  return part;
 }
 
 export async function POST(req: Request) {
-    // Extract the `prompt` from the body of the request
-    const { messages, data } = await req.json();
+  // Extract the `prompt` from the body of the request
+  const { messages, data } = await req.json();
 
-    const currentMessage = messages[messages.length - 1];
-    const prompt = currentMessage.content;
-    const image = fileToGenerativePart("app/sample_outfit.jpeg", "image/jpeg");
-    const textPart = {text: prompt};
-    const request = {
-        contents: [{role: 'user', parts: [textPart, image]}],
-    };
-    
-    const geminiStream = await gemini_model.generateContentStream(request);
+  const currentMessage = messages[messages.length - 1];
 
-    // Convert the response into a friendly text-stream
-    const stream = GoogleGenerativeAIStream(geminiStream);
-    // Respond with the stream
-    return new StreamingTextResponse(stream);
+  // Prompt that gets sent to the model
+  const prompt = currentMessage.content;
+  console.log(prompt);
+  const textPart = { text: prompt };
+
+  // fileToGenerativePart(image, image_type)
+  // const image = fileToGenerativePart("app/sample_outfit.jpeg", "image/jpeg");
+
+  // Image part that gets sent to the model
+  const imagePart = {
+    inlineData: {
+      data: data.imageUrl,
+      mimeType: "image/jpeg",
+    },
+  };
+
+  const request = {
+    contents: [{ role: "user", parts: [textPart, imagePart] }],
+  };
+
+  const geminiStream = await gemini_model.generateContentStream(request);
+
+  // Convert the response into a friendly text-stream
+  const stream = GoogleGenerativeAIStream(geminiStream);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,57 @@
-'use client';
- 
-import { useChat } from 'ai/react';
- 
+"use client";
+
+import { useChat } from "ai/react";
+import { useState } from "react";
+
+
+
+
+function processBase64Data(data){
+  const modifiedData = data.replace("data:image/jpeg;base64,", "")
+  return modifiedData;
+}
+
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
- 
+  const [uploadedImage, setImage] = useState(null);
+  const [processedData, setProcessedData] = useState(null);
+  const [uploadedImageFile, setImageFile] = useState(null);
+
+  const onImageChange = (event) => {
+    const file = event.target.files[0];
+    var reader = new FileReader();
+    reader.onload = function () {
+      var base64data = reader.result;
+      const processedData = processBase64Data(base64data)
+      setProcessedData(processedData)
+    };
+    reader.readAsDataURL(file);
+    if (event.target.files && event.target.files[0]) {
+      const i = event.target.files[0];
+      setImage(URL.createObjectURL(i));
+    }
+  };
+
+
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+      <input type="file" onChange={onImageChange} />
       <p>Sample Image</p>
-      <img 
-      src='https://cdn.cliqueinc.com/posts/298692/streetwear-outfits-for-women-298692-1648082641136-main.700x0c.jpg'/>
+      <img src={uploadedImage} />
       {messages.length > 0
-        ? messages.map(m => (
+        ? messages.map((m) => (
             <div key={m.id} className="whitespace-pre-wrap">
-              {m.role === 'user' ? 'User: ' : 'AI: '}
+              {m.role === "user" ? "User: " : "AI: "}
               {m.content}
             </div>
           ))
         : null}
- 
+
       <form
-        onSubmit={e => {
+        onSubmit={(e) => {
           handleSubmit(e, {
             data: {
-              imageUrl:
-                'https://cdn.cliqueinc.com/posts/298692/streetwear-outfits-for-women-298692-1648082641136-main.700x0c.jpg',
+              imageUrl: processedData,
             },
           });
         }}
