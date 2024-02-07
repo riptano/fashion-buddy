e16535e1939e8e6aabb8e369fd5741d78069e8ae
Mike Fortman <michael.fortman@datastax.com>
2024-02-07 09:24:22 -0600

    spacing

--- components/Product.tsx
+++ components/Product.tsx
@@ -10,12 +10,15 @@ const Product = ({ item }: Props): JSX.Element => {
 
   return (
     <div className="flex">
-      <Image src={item.product_images} alt={item.product_name} height={150} width={100} />
+      <Image className="mr-2" src={item.product_images} alt={item.product_name} height={150} width={100} />
       <div className="flex flex-col flex-1">
         <Link href={item.link}>{item.product_name}</Link>
         <p>{item.details}</p>
         <p>{item.price}</p>
-        <p>{item.$similarity}</p>
+        <div className="flex ">
+          <span>{item.$similarity * 100}%</span>
+          <button>Buy</button>
+        </div>
       </div>
     </div>
   )
