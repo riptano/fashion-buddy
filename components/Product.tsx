fa963a2f152b99c7d732f0caffc6e4f8accd42d9
Mike Fortman <michael.fortman@datastax.com>
2024-02-12 17:14:00 -0600

    redesign products page

--- components/Product.tsx
+++ components/Product.tsx
@@ -9,19 +9,28 @@ interface Props {
 
 const Product = ({ item }: Props): JSX.Element => {
 
+  const formatTitle = (str: string) => {
+
+    return str.toLowerCase().replace(/\b\w/g, function(char) {
+      return char.toUpperCase();
+    });
+  }
+
   return (
     <div className="flex w-full pt-2">
       <Image className="mr-2" src={item.product_images} alt={item.product_name} height={125} width={125} />
       <div className="flex flex-col flex-1 justify-between p-4">
-        <Link href={item.link}>{item.product_name}</Link>
-        <p>{item.details}</p>
-        <p>${item.price}</p>
-        <div className="flex justify-between">
-          <span className="p-2 radius-full">{ Math.round(item.$similarity * 1000)/10 }% Match</span>
-          <button className="flex items-center p-2 radius-full">
-            <Cart />
-            Buy
-          </button>
+        <h5 className="text-base font-semibold">{formatTitle(item.product_name)}</h5>
+        <p className="text-sm">{item.details}</p>
+        <p className="text-lg font-bold">${item.price}</p>
+        <div className="flex justify-between text-xs">
+          <span className="p-2 rounded-full bg-white">{ Math.round(item.$similarity * 1000)/10 }% Match</span>
+          <Link href={item.link} rel="noopener noreferrer" target="_blank">
+            <button className="slime-background flex items-center p-2 gap-2 rounded-full">
+              <Cart />
+              Buy
+            </button>
+          </Link>
         </div>
       </div>
     </div>
