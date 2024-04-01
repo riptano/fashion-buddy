895f6a49fdc6c8adeb985631020aa6b7bf434977
Deon Sanchez <deon.sanchez@datastax.com>
2024-04-01 17:11:05 -0600

    Refactor page layout for responsive design

--- app/page.tsx
+++ app/page.tsx
@@ -20,9 +20,9 @@ export default function Home() {
   return (
     <div className="flex min-h-screen cream-background">
       {/* Left side */}
-      <div className="flex-1 bg-cover bg-center clothing-background">
+      <div className="md:flex-1 md:bg-cover sm:bg-center md:flex clothing-background ">
         {/* Logo and powered by text */}
-        <div className="p-10">
+        <div className="lg:p-10 md:p-8 p-6 z-10 absolute">
           <h3 className="text-black">Powered by</h3>
           <Image
             className="mt-2"
@@ -35,20 +35,22 @@ export default function Home() {
       </div>
 
       {/* Right side */}
-      <div className="flex-1 flex justify-center items-center">
+      <div className="flex-1 flex justify-center items-center bg-cover bg-center sm:bg-cover sm:bg-center clothing-background md:bg-none">
         {/* Content container */}
-        <div className="grid gap-6">
+        <div className="grid gap-6 grid-rows-1 place-items-start px-6">
           {/* Main heading */}
-          <p className="text-6xl font-bold">Fashion, Meet AI.</p>
+          <p className="md:text-6xl text-5xl font-bold md:text-left text-center">
+            Fashion, Meet AI.
+          </p>
 
           {/* Subheading */}
-          <p className="text-xl max-w-xs">
+          <p className="text-xl text-center md:text-left">
             Say goodbye to wardrobe dilemmas and hello to effortless style.
           </p>
 
           {/* "Get started" button */}
           <button
-            className="slime-background flex items-center justify-center rounded-full text-lg font-semibold mt-6 w-full max-w-sm px-6 py-3"
+            className="slime-background flex items-center justify-center rounded-full text-lg font-semibold mt-3 w-full px-6 py-3"
             onClick={handleOpenDialog}
           >
             Get started
